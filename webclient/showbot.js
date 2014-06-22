9ffe6aa68a21061d9e1e135b156e17586d22d7dc
Casey Liss <casey@caseyliss.com>
2014-06-22 07:15:14 -0400

    remove annoying PONG message.

--- webclient/showbot.js
+++ webclient/showbot.js
@@ -127,7 +127,6 @@ Showbot.Bot = (function ($) {
                     var span = $(row).find('.votes').html(packet.votes);
                 } else if (packet.operation == 'PONG') {
                     // NOOP
-                    console.log('PONG');
                 }
 			};
 
